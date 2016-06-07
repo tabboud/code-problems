32519dd77e52c9f5343c3fa794713c8aeb41587b
Tony Abboud <tdabboud@hotmail.com>
2016-06-07 09:12:54 -0400

    WIP: top scores

---
+++ problems/interview_cake/32_top_scores/top_scores.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Top Scores
+"""
+
+def top_scores(unsorted_scores, highest_possible_score):
+    """ Return a sorted list of scores in less than O(nlogn) time
+
+    Complexity:
+        Time  -> O():
+        Space -> O():
+    """
+    pass
+
+
+if __name__ == "__main__":
+    scores= [30, 50, 20, 40, 10]
+    highest_possible = 50
+    print top_scores(scores, highest_possible)
