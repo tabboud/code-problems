64faa487982b6964aadef857686a94cfac434d36
Tony Abboud <tdabboud@hotmail.com>
2016-06-11 17:51:48 -0400

    check if linked list is a permutation using a slow and fast pointer

--- problems/linked_list_palindrome/linked_list_palindrome.py
+++ problems/linked_list_palindrome/linked_list_palindrome.py
@@ -16,6 +16,44 @@ def create_linked_list(values):
 
     return root
 
+def is_permutation_slow_fast(root):
+    """ Check if the linked list is a permutation by using a stack
+        and a slow and fast pointer
+
+    Complexity:
+        Time  -> O(n): Iterate the list once
+        Space -> O(n): Create an n/2 element stack
+    """
+    # use a stack to compare the last elements to the first elements
+    # This is the same way recursion would work
+    stk = []
+    # keep track of the length of the list as we traverse with the fast pointer
+    length = 1
+
+    slow = root
+    fast = root
+    while fast is not None:
+        stk.append(slow.value)
+        slow = slow.next
+        if fast.next is not None:
+            length += 2
+            fast = fast.next.next
+        else:
+            break
+
+    # Handle the case where the length is odd so the
+    # middle character is the rotation
+    if length % 2 != 0:
+        stk.pop()
+
+    # Keep moving the slow pointer and check it against the stack
+    while slow is not None:
+        if slow.value != stk.pop():
+            return False
+        slow = slow.next
+
+    return True
+
 
 def is_permutation_stack(root):
     """ Check if the linked list is a permutation by using a stack
@@ -64,4 +102,5 @@ if __name__ == "__main__":
     root = create_linked_list('racecar')
     print is_permutation_stack(root)
     print is_permutation_list(root)
+    print is_permutation_slow_fast(root)
 
